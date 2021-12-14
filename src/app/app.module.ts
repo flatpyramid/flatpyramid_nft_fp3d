bf0fc34a565daeebb3e664e37f9834790c70e147
Esther Villars <esthervillars@gmail.com>
2021-12-14 12:40:42 -0800

    Add GA, FA and save 3rd party deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoogleChartsModule } from 'angular-google-charts';
 import { BrowserModule } from '@angular/platform-browser';
 import Web3 from 'web3';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { PresaleComponent } from './presale/presale.component';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
@@ -21,7 +24,9 @@ import { PresaleComponent } from './presale/presale.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    GoogleChartsModule
+    GoogleChartsModule,
+    NgxGoogleAnalyticsModule.forRoot(environment.ga),
+    NgxGoogleAnalyticsRouterModule
   ],
   providers: [Web3],
   bootstrap: [AppComponent]
